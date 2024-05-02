d2ad2344e8c1c7f43c036ac275fb4df200373720
jshprz <joshuaperezmelgazo@gmail.com>
2024-05-02 17:17:31 +0800

    Header and Hero section implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="thebackground">
+      <Header />
+      <Hero />
+    </div>
   )
 }
