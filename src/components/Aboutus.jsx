5c9739dda090958eccab171d9cb3f0e2900bd39f
jshprz <joshuaperezmelgazo@gmail.com>
2024-05-30 23:50:20 +0800

    Adding about us component

---
+++ src/components/Aboutus.jsx
@@ -0,0 +1,14 @@
+export default function Aboutus() {
+    return (
+        <>
+            <div className="flex">
+                <div>
+                    <p>About Us</p>
+                </div>
+                <div>
+                    <p>Test</p>
+                </div>
+            </div>
+        </>
+    )
+}  
