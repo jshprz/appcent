48b017b4ab99477dadfb52af29096356786efeb1
jshprz <joshuaperezmelgazo@gmail.com>
2024-05-31 23:50:23 +0800

    Adding why us component

---
+++ src/components/Whyus.jsx
@@ -0,0 +1,14 @@
+export default function Whyus() {
+    return (
+        <>
+            <div className="flex">
+                <div>
+                    <p>Why Us?</p>
+                </div>
+                <div>
+                    <p>Test</p>
+                </div>
+            </div>
+        </>
+    )
+}  
